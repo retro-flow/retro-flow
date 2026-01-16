1db385dfb2d54a32e91a751eeaa6fbbf2f3caaef
yarastqt <yarastqt@gmail.com>
2026-01-16 21:40:19 +0300

    feat: disconnect pg pool when module is destroy

--- backend/board/src/prisma-service.ts
+++ backend/board/src/prisma-service.ts
@@ -1,13 +1,21 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, type OnModuleDestroy, type OnModuleInit } from '@nestjs/common'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 import { PrismaClient } from '@app/prisma/client'
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   constructor() {
     const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 
     super({ adapter })
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
   }
 }
